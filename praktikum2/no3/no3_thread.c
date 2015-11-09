bb1c274da0cb0d8e79d2303dede0b24ee0b0f73c
setyassida <setyassida@gmail.com>
2015-11-09 15:56:46 +0700

    tadi ada yg error kurang ;, sekarang masih error juga

--- praktikum2/no3/no3_thread.c
+++ praktikum2/no3/no3_thread.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 #include <pthread.h>
 
-void func1()
+void *func1()
 {
 	FILE *rf, *wf;
 	char c;
 	rf=fopen("file1.txt", "r");
-	wf=fopen("file2.txt", "w")
+	wf=fopen("file2.txt", "w");
 	while((c=fgetc(rf))!=EOF)
 	{
-		fprintf(wf, "%c", c)
+		fprintf(wf, "%c", c);
 	}
 	fclose(rf);
 	fclose(wf);
-	return 0;
+	//return 0;
 }
 
-void func2()
+void *func2()
 {
 	FILE *rf, *wf;
 	char c;
 	rf=fopen("file2.txt", "r");
-	wf=fopen("file3.txt", "w")
+	wf=fopen("file3.txt", "w");
 	while((c=fgetc(rf))!=EOF)
 	{
-		fprintf(wf, "%c", c)
+		fprintf(wf, "%c", c);
 	}
 	fclose(rf);
 	fclose(wf);
-	return 0;
+	//return 0;
 }
 
 void main(){
@@ -36,10 +36,10 @@ void main(){
 	pthread_create(&t1, NULL, func1, NULL);
 	pthread_create(&t2, NULL, func2, NULL);
 
-	pthread_join(t1, NULL);
+	pthread_join(&t1, NULL);
 	/*int i=0;
 	for(i=1;i<30;i++) printf("Main : %d\n", i);
 	*/
-	pthread_join(t2, NULL);
-	return 0;
+	pthread_join(&t2, NULL);
+	//return 0;
 }
