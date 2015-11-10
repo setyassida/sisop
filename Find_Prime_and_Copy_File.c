77353e62445c10f059cef3bc1c4c1023dfb803a9
jeffrynasri <jeffryfaruki@gmail.com>
2015-11-10 17:20:53 +0700

    Perbaikan pthread join

--- Find_Prime_and_Copy_File.c
+++ Find_Prime_and_Copy_File.c
@@ -18,29 +18,25 @@ void *find_prime(void *args) {
 	}
   	pthread_exit(NULL); // Kalau tidak ada ini, jika maks-nya 2 maka return nya 2 kali
 }
-void *copy1(void *args) {
-	 //char *arg[] = {"/bin/ls", "-r", "-t", "-l", (char *) 0 };
-	//execv("/bin/ls", arg);
-	execl( "/bin/ls", "-r", "-t", "-l", (char *) 0);
-}
-//void *copy2(void *args) {
 
-//}
 void main () {
-	int input,i=1,banyak_bilprim =0;
-	printf("Input some number : ");scanf("%d",&input);	
+	int input,i=1,j=0,banyak_bilprim =0;
+	printf("Input some number : ");scanf("%d",&input);
 	pthread_t t1[input];
 	void* bilprim;
-	while(i <= input)
-    	{
-        	pthread_create(&(t1[i]), NULL, find_prime, (void*)i);
-		pthread_join(t1[i], &bilprim);
-		if(bilprim!=0){
-			printf("%d ",(int)bilprim);       
-			banyak_bilprim++;
-		}		
-
+	while(i <= input){
+        pthread_create(&(t1[i]), NULL, find_prime, (void*)i);
 		i++;
 	}
+	while(j <= input){
+        pthread_join(t1[j], &bilprim);
+		if(bilprim!=0){
+			printf("%d ",(int)bilprim);
+			banyak_bilprim++;
+        }
+        j++;
+	}
+
 	printf("\nTerdapat %d bilangan prima \n",banyak_bilprim);
+
 }
